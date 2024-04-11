2fcae37d8cafc34a3b936946332e04a4b62cd3a8
Yipmong Said <Syipmong5@gmail.com>
2024-04-11 19:47:18 +0100

    Add project files.

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+
+import 'bootstrap/dist/css/bootstrap.css';
+import PrimarySearchAppBar from './components/Header';
+
+function App() {
+    return <PrimarySearchAppBar/>;
+    
+
+}
+
+
+export default App;
---
+++ src/components/modules/Button.tsx
@@ -0,0 +1,9 @@
+import Button from '@mui/material/Button';
+
+
+function ButtonUsage() {
+    return <Button variant="contained">Hello world</Button>;
+
+}
+
+export default ButtonUsage;
